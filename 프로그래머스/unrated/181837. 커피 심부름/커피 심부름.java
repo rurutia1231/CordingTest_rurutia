ed321a095c3233ea2d6a3a21e6af735e13bc49e8
yoon young min <123915394+rurutia1231@users.noreply.github.com>
2023-08-22 22:58:08 +0900

    [unrated] Title: 커피 심부름, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -1,19 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(String[] order) {
-        String[] price4500 = {"iceamericano", "americanoice", "hotamericano", "americanohot", "americano", "anything"};
-        String[] price5000 = {"icecafelatte", "cafelatteice", "hotcafelatte", "cafelattehot", "cafelatte"};
-
-        int total = 0;
-        
-        for(String item : order) {
-            if(Arrays.stream(price4500).anyMatch(menu -> menu.equals(item))) {
-                total += 4500;
-            } else {
-                total += 5000;
-            }
+        int answer = 0;
+        for (String eachOrder : order) {
+            if (eachOrder.contains("americano") || eachOrder.equals("anything"))
+                answer += 4500;
+            else 
+                answer += 5000;
         }
-        return total;
+        return answer;
     }
 }
